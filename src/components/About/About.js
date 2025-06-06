d79fe1b0a3ea23886227899a426a7317a1b00f97
sohkachun <sohkachun@yahoo.com>
2025-06-05 22:19:43 -0400

    update file

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
-import Github from "./Github";
+
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/about.png";
-import Toolstack from "./Toolstack";
+
 
 function About() {
   return (
