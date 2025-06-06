c3156b9c231b7b9c1a4599f391787bb11e00d698
Casey Soh <119526233+sohkachun@users.noreply.github.com>
2025-06-05 22:05:55 -0400

    Add files via upload

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  DiPython,
+  DiGit,
+  DiDocker,
+  DiMysql,
+  DiPostgresql,
+} from "react-icons/di";
+import {
+  SiPytorch,
+  SiTensorflow,
+  SiGooglecloud,
+  SiTableau,
+  SiPowerbi,
+  SiPostman,
+} from "react-icons/si";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPytorch />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTensorflow />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiDocker />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPostgresql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMysql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPowerbi />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTableau />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiGooglecloud />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
+
